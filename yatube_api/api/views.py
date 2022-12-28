ad559c71b5a575972749eb0cf9a0c930932ce358
Brideshead <alexander.bogoevich@gmail.com>
2022-12-28 12:38:26 +0100

    final3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (
-    IsAuthenticated, 
+    IsAuthenticated,
     IsAuthenticatedOrReadOnly,
 )
 
